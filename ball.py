ab4833f777667045633974bb16a54825a1276351
AlexNol03 <136399183+AlexNol03@users.noreply.github.com>
2023-06-13 17:43:13 -0700

    Add files via upload

---
+++ ball.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#  Program for computing the height of a ball in vertical motion
+# v0 is the intial velocity, g is the force of gravity, t is time, y is the maximum height
+v0 = 45; g = 32.8; t = 0.9
+
+y = v0*t - 0.5*t**2
+
+print (y)
